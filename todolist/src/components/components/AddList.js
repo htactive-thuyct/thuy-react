b5d8360ceb17910edfc2e7981d109ee535ab823a
thuy.cao <thuyct@htactive.com>
2019-07-08 17:40:35 +0700

    todolist use API

---
+++ todolist/src/components/components/AddList.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+
+export default class AddList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { newTask: "" };
+  }
+
+  handleChange = event => {
+    this.setState({ newTask: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.addItem(this.state.newTask);
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <div className="content-area row">
+          <div className="col-md-2 col-xs-2 col-lg-2 col-sm-2" />
+          <div className="col-md-8 col-xs-8 col-lg-8 col-sm-8">
+            <h2>ToDo List</h2>
+            <form onSubmit={this.handleSubmit}>
+              <div className="row input-area">
+                <div className="form-group col-md-9">
+                  <input
+                    type="text"
+                    placeholder="New Task"
+                    className="form-control"
+                    id="addTask"
+                    value={this.state.newTask}
+                    onChange={this.handleChange}
+                  />
+                </div>
+                <div className="form-group col-md-1">
+                  <input
+                    className="btn btn-primary"
+                    type="submit"
+                    value="Submit"
+                  />
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
